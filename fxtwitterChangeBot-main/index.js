048fe0ca2b8f290b859ced3a76718519e6d15e0a
aru-huss <emoji910daisuki@gmail.com>
2023-12-23 22:19:07 +0900

    URL以外のテキストを表示しないようにしました

--- fxtwitterChangeBot-main/index.js
+++ fxtwitterChangeBot-main/index.js
@@ -34,19 +34,46 @@ client.on("messageCreate", async (message) => {
   // メッセージが "https://twitter.com/username/status/xxxxxx" または "https://x.com/username/status/xxxxxx" の形式を確認する。
   if (content.match(/https:\/\/(twitter\.com|x\.com)\/[^/]+\/status\/\d+/)) {
     try {
+      //console.log("try \n",);   //fordebug
+      var count = 0;      
+      var replace_char,temp_char;
+      replace_char = content;
+      while(temp_char!=replace_char){
+        temp_char = replace_char;
+        replace_char = replace_char.replace(/\r|\n/g, ' ');           //タブ・改行コードを変換
+        replace_char = replace_char.replace(/[\s\u{3000}]/ug,' ');    //空白大文字を空白小文字に変換
+        //console.log("タブなど変換\n",replace_char);
+      }
+      var replace_char2,temp_char2;
+      replace_char2 = replace_char;
+      while (temp_char2!=replace_char2){
+        temp_char2 = replace_char2;
+        var replace_char2 = replace_char2.replace('  ',' ')           //空白を減らしていく
+       // console.log("空白変換\n",replace_char2);
+      }
+      const split_char = replace_char2.split(' ');                    //空白を区切りに配列に格納
+      //console.log("try2 \n",split_char);                            //fordebug
+      for(let i=0;i<split_char.length;i++){
+        if(split_char[i].match(/https:\/\/(twitter\.com|x\.com)\/[^/]+\/status\/\d+/)){
+          // twitter.comの場合は、fxtwitter.comに変更し、x.comの時は、fixupx.comに変更する。
+          const updatedContent = split_char[i]
+          .replace(/https:\/\/twitter\.com/g, "https://fxtwitter.com")
+          .replace(/https:\/\/x\.com/g, "https://fixupx.com");
+          const newMessage = `${updatedContent}`;
+          message.channel.send(newMessage);
+          count++;
+          console.log("",count);
+        }else {
+          //console.log("else \n",split_char[i]);   //fordebug
+        }
+      }
 
-      // twitter.comの場合は、fxtwitter.comに変更し、x.comの時は、fixupx.comに変更する。
-      const updatedContent = content
-        .replace(/https:\/\/twitter\.com/g, "https://fxtwitter.com")
-        .replace(/https:\/\/x\.com/g, "https://fixupx.com");
-
-      const newMessage = `${updatedContent}`;
-      message.channel.send(newMessage);
+    
     } catch (error) {
       console.error("メッセージの処理中にエラーが発生しました:", error);
     }
   } else {
-    console.log("", content);
+    //console.log("", content);
   }
 });
 
